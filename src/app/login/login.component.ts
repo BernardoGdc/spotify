97908ae8963f303d0db0563274dfc10bd5128e70
Bernardo Glz de Castilla <67803645+BernardoGdc@users.noreply.github.com>
2024-05-11 20:12:54 -0500

    enable authorization

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginURL = `https://accounts.spotify.com/authorize?client_id=${environment.client_id}&redirect_uri=${environment.redirect_url}&response_type=code`
 
+  constructor(private http: HttpClient) {
+
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    client_id: "70f58a425bb649559ded170db702bea5",
+    client_secret: "6204389d4f6d4528a75dededa65a1996",
+    redirect_url: "http://localhost:4200/",
+    
+};
